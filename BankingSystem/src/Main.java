1af1bf67c65e9e620ad8d01bcf737fb985618d86
Joel Christopher <118204275+joelchristopher-d@users.noreply.github.com>
2024-11-28 23:43:30 +0530

    Update Main.java

--- BankingSystem/src/Main.java
+++ BankingSystem/src/Main.java
@@ -7,9 +7,12 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        Actions Create = new Actions();
+        Transaction Create = new Transaction();
+        Create.CreateAccount("Joel",10000,"ABab12");
+        Create.CreateAccount("Leo",20000,"AB123ab");
+        Create.CreateAccount("Doggy",40000,"JoE1234");
         while (true){
-            System.out.println("Welcome to Dawson's Banking Application");
+            System.out.println("\n---------------------Welcome to Dawson's Banking Application---------------------\n");
             System.out.println("1.CreateAccount \n" +
                                 " 2.Login \n" +
                                 "3.Exit");
@@ -22,7 +25,7 @@ public class Main {
                 String pwd = scan.next();
                 Create.CreateAccount(Name,10000,pwd);
             } else if (input == 2) {
-                System.out.println("Enter Your AccntNo");
+//                System.out.println("Enter Your AccntNo");
                 String AccNo = Create.Login(new Scanner(System.in));
                 if(!Objects.equals(AccNo, "0")){
                     System.out.println("1.ATM withdrawal \n" +
@@ -31,8 +34,20 @@ public class Main {
                                         "4.Check Transaction History\n"+
                                         "5.Password change");
                     int in = scan.nextInt();
-                    if(in==5){
+                    if(in==1){
+                        Create.ATMWithdraw(new Scanner(System.in),AccNo);
+                    } else if (in==2) {
+                        Create.Cashdeposit(new Scanner(System.in),AccNo);
+                    } else if (in==3) {
+                        Create.Transfer(new Scanner(System.in),AccNo);
+                    }
+                    else if (in == 4) {
+                        Create.viewTransactionHistory(new Scanner(System.in),AccNo);
+                    }
+                    else if(in==5){
                         Create.PasswordChange(new Scanner(System.in),AccNo);
+                    } else if (in == 6) {
+                        Create.viewPwdHistory(new Scanner(System.in),AccNo);
                     }
 
 
@@ -40,7 +55,6 @@ public class Main {
 
             }
 
-            break;
         }
 
     }
